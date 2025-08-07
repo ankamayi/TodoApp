052b3cc655c03baa543d07391386858a677ddcf7
ankamayi <drupalinside@gmail.com>
2025-08-07 20:27:32 +0900

    post機能追加

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,28 @@ import { PrismaClient } from "@prisma/client";
 const app:  Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const prisma = new PrismaClient();
 
 app.get("/allTodos", async (req: Request, res: Response) => {
     const allTodos = await prisma.todo.findMany();
     return res.json(allTodos);
+});
+
+app.post("/createTodo", async (req: Request, res: Response) => {
+    try {
+        const { title, isCompleted } = req.body;
+        const createTodo = await prisma.todo.create({
+            data: {
+                title: title,
+                isCompleted: isCompleted,
+            },
+         });
+         //成功した場合は、作成されたTODOをJSON形式で返す
+        return res.json(createTodo);
+    } catch (e) {
+        return res.status(500).json({ error: "Failed to create todo"});
+    }
 });
 
 app.listen(PORT,() => console.log("server is running🚀"));
